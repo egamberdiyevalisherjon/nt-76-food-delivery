d485cfe00e32305b883ab1d3c80866a860008fbe
SaidqodirxonUz <115058529+Saidqodirxon@users.noreply.github.com>
2022-11-07 09:55:15 +0500

    Hikoya Sayt

---
+++ Desktop/Hikoya/script.js
@@ -0,0 +1,88 @@
+alert(
+  "Salom,  Hozir siz bilan ajoyib hikoya tuzamiz, Sizga 12 ta savol beraman. Sizdan iltimosim, barcha savollarga javob bering (Har bir savolga javob yozgandan so'ng OK yoki 'Enter' ni bosing). Tayyormisiz? Qiziq Bo'lsa boshlaymiz Yoqmasa Oldindan Uzraa bu shunchaki JavaScript Test "
+);
+ism = prompt("ismingiz nima ?", "bu yerga yozing");
+familiya = prompt("Familiyangiz nima ?", " ");
+yosh = prompt("Yoshingiz nechada ?", " ");
+yashash = prompt("Qaysi shaharda yashaysiz ?", " ");
+birodar = prompt("Sizning eng yaqin dostingiz kim (ismini yozing)?", " ");
+tanishuv = prompt("O'sha do'stingiz bilan qayerda tanishgansiz ?", " ");
+shahar = prompt("Oxirgi marta qaysi shaharga bordingiz ?", " ");
+transport = prompt("O'sha shaharga qaysi transportda bordingiz ?", " ");
+rang = prompt("O'sha transportning rangi taxminan qanday edi ?", " ");
+qaroqchi = prompt("Yo'lda kimni uchratgan edingiz, uning ismi nima edi ?", " ");
+dedi = prompt("Siz o'sha uchratgan odamingizga nima dedingiz ?", " ");
+hayvon = prompt("Yo'lda qaysi hayvonga duch kelgan edingiz ?", " ");
+document.write(
+  "Bir bor ekan bir yo'q ekan, Hozirgi " +
+    yashash +
+    " degan shaharda " +
+    ism +
+    " degan odam bor ekan. Uning familiyasi " +
+    familiya +
+    " ekan. Aytishlaricha hozir uning yoshi " +
+    yosh +
+    " da ekan. Kunlardan bir kuni " +
+    ism +
+    " " +
+    tanishuv +
+    " degan joyda " +
+    birodar +
+    " bilan tanishibdi. " +
+    ism +
+    " va " +
+    birodar +
+    " do'st bo'lishibdi. Bir kuni " +
+    ism +
+    " bilan " +
+    birodar +
+    " birgalikda " +
+    shahar +
+    "ga borishga qaror qilishibdi. Ular " +
+    rang +
+    " " +
+    transport +
+    "ga minib yo'lga chiqibdi. Uzoq yo'l bosibdi. Yo'lda " +
+    transport +
+    " buzilibdi. Shu payt " +
+    ism +
+    " va " +
+    birodar +
+    " oldiga baland bo'yli " +
+    qaroqchi +
+    " paydo bo'lib Ularning yo'lini to'sibdi. Aslida " +
+    qaroqchi +
+    " qaroqchilik qilar ekan. Shunda " +
+    ism +
+    " " +
+    qaroqchi +
+    "ga qarab " +
+    dedi +
+    " - debdi. Buni eshitgan qaroqchi -" +
+    qaroqchi +
+    "ning g'azabi kelib " +
+    ism +
+    "ga qilich ko'taribdi. Shu payt " +
+    ism +
+    "ning baxtiga uzoqdan bahaybat yirtqich " +
+    hayvon +
+    " paydo bo'ldi va u yugurib " +
+    qaroqchi +
+    " oldiga keldi. Keyin esa " +
+    qaroqchi +
+    "ni g'ajib tashlab o'ljaga aylantiribdi. Buni ko'rgan " +
+    ism +
+    " va " +
+    birodar +
+    " xursand bo'dilar. Chunki " +
+    hayvon +
+    " ularni " +
+    qaroqchi +
+    "dan xalos etgandi. Shunday qilib har ikki do'st: " +
+    familiya +
+    " " +
+    ism +
+    " va " +
+    birodar +
+    " murodu maqsadiga yetgan ekan..."
+);
